5c8ae06f46808bd3f98b47dc531c748850efc43b
Domen <domen.kosir@ineor.si>
2024-05-23 10:48:36 +0200

    Adding DbContext, Migrations...

---
+++ CarApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CarApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Car> Cars { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Car>()
+                .Property(c => c.Price)
+                .HasPrecision(18, 2);
+
+        }
+    }
+}
